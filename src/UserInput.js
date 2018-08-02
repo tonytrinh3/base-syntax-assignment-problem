c8d29987391f622cefd7b3098a04b79f8c52ce9d
Tony Trinh <tony.w.trinh3@gmail.com>
2018-08-01 21:19:23 -0700

    2018-08-01 start of base syntax assignment problem
    2018-08-01 start of base syntax assignment problem

---
+++ src/UserInput.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+const UserInput = () => {
+    return (
+        <div>
+           <p>hi</p>
+           <input type = "text">
+        </div>
+    )
+}
+
+ 
+export default UserInput
---
+++ src/UserOutput.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+const UserOutput = () => {
+    return(
+        <div>
+            <p></p>
+            <p></p>
+        </div>
+    )
+}
+
+export default UserOutput
